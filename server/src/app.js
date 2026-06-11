063a5db0916f1809399c2ddf3e09508085f09e3d
Krish <khambhatikrish@gmail.com>
2026-06-11 13:46:36 +0530

    create morgan setup

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,13 @@
 let express = require("express")
-
+let morgan = require("morgan")
+let env = require("./config/env")
 
 let createApp = () => {
     let app = express()
 
+    if (env.NODE_ENV === "developement") {
+        app.use(morgan("dev"))
+    }
 
     return app
 }
--- server/src/config/env.js
+++ server/src/config/env.js
@@ -3,7 +3,8 @@ let zod = require("zod")
 
 let envSchema = zod.object({
     PORT: zod.coerce.number(),
-    MONGO_URL: zod.string()
+    MONGO_URL: zod.string(),
+    NODE_ENV: zod.string(),
 })
 
 const parsed = envSchema.safeParse(process.env);
