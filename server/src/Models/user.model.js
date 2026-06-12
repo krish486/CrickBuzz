e41817362a49b893cd2b4ee4f80ac129b9486f68
Krish <khambhatikrish@gmail.com>
2026-06-12 22:47:20 +0530

    limit of 3mb  added for body-data/form-data

--- server/src/Models/user.model.js
+++ server/src/Models/user.model.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema(
     {
         name: { type: String, required: true, trim: true },
         email: { type: String, required: true, unique: true, lowercase: true, trim: true },
-        password: { type: String, required: true },
+        password: { type: String },
         role: { type: String, enum: Object.values(ROLES), default: ROLES.SCORER },
         isDeleted: { type: Boolean, default: false },
     },
--- server/src/app.js
+++ server/src/app.js
@@ -5,6 +5,9 @@ let env = require("./config/env")
 let createApp = () => {
     let app = express()
 
+    app.use(express.json({ limit: "3mb" }))
+    app.use(express.urlencoded({ extended: true, limit: "3mb" }))
+
     if (env.NODE_ENV === "developement") {
         app.use(morgan("dev"))
     }
