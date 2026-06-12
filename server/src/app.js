14157b42d958ac8d7c45afb1cbed53412e54ceb4
Krish <khambhatikrish@gmail.com>
2026-06-12 23:09:10 +0530

    create security headers

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,17 @@
 let express = require("express")
 let morgan = require("morgan")
 let env = require("./config/env")
+let hpp = require("hpp")
+let helmet = require("helmet")
+let compression = require("compression")
+
 
 let createApp = () => {
     let app = express()
+
+    app.use(helmet)
+    app.use(hpp)
+    app.use(compression)
 
     app.use(express.json({ limit: "3mb" }))
     app.use(express.urlencoded({ extended: true, limit: "3mb" }))
