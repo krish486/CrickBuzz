849dc1c60c7c55ba2011ac00f5ffc2c2d8c51126
Krish <khambhatikrish@gmail.com>
2026-06-18 10:55:05 +0530

    basic setup of store done

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,9 +4,13 @@ import { RouterProvider } from "react-router"
 import router from './App'
 import { QueryClientProvider, } from '@tanstack/react-query'
 import queryClient from './lib/queryClient'
+import { Provider } from "react-redux"
+import { store } from './lib/store'
 
 createRoot(document.getElementById('root')).render(
   <QueryClientProvider client={queryClient}>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </QueryClientProvider>
 )
