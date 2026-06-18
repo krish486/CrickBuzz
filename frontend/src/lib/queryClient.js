153524cd2efc0d4b2741fe8b6f1bb835794b641a
Krish <khambhatikrish@gmail.com>
2026-06-18 10:47:51 +0530

    basic set of query client done

---
+++ frontend/src/lib/queryClient.js
@@ -0,0 +1,9 @@
+import {
+    QueryClient,
+} from '@tanstack/react-query'
+
+
+// Create a client
+const queryClient = new QueryClient()
+
+export default queryClient
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from "react-router"
 import router from './App'
+import { QueryClientProvider, } from '@tanstack/react-query'
+import queryClient from './lib/queryClient'
 
 createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />
+  </QueryClientProvider>
 )
